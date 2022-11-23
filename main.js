f5b730fca344c1f13813c5c2734ea3a837a4fc94
Abdurauf1 <aabdurauf004@gmail.com>
2022-11-23 23:47:21 +0500

    fix: some navbar styles

---
+++ main.js
@@ -0,0 +1,13 @@
+// responsive navbar
+// variables
+const nav = document.querySelector("nav");
+const openNav = document.querySelector(".nav-open-btn");
+const closeNav = document.querySelector(".close-nav-btn");
+
+openNav.addEventListener("click", () => {
+    nav.classList.add("nav-modal")
+})
+
+closeNav.addEventListener("click", () => {
+    nav.classList.remove("nav-modal")
+})
