27f9bd7271ac92c66b868440d90ff08e5c5e91a8
Abdurauf1 <aabdurauf004@gmail.com>
2022-11-27 00:43:10 +0500

    fix: bug fixes

--- main.ts
+++ main.ts
@@ -4,10 +4,10 @@ const nav = document.querySelector("nav");
 const openNav = document.querySelector(".nav-open-btn");
 const closeNav = document.querySelector(".close-nav-btn");
 
-openNav.addEventListener("click", () => {
-  nav.classList.add("nav-modal");
+openNav?.addEventListener("click", () => {
+  nav?.classList.add("nav-modal");
 });
 
-closeNav.addEventListener("click", () => {
-  nav.classList.remove("nav-modal");
+closeNav?.addEventListener("click", () => {
+  nav?.classList.remove("nav-modal");
 });
