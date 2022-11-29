711ecb5cfd5d082c95d0042c9329e8659e463712
Abdurauf1 <aabdurauf004@gmail.com>
2022-11-29 06:32:34 +0500

    created public folder

---
+++ public/main.js
@@ -0,0 +1,12 @@
+"use strict";
+// responsive navbar
+// variables
+const nav = document.querySelector("nav");
+const openNav = document.querySelector(".nav-open-btn");
+const closeNav = document.querySelector(".close-nav-btn");
+openNav.addEventListener("click", () => {
+    nav.classList.add("nav-modal");
+});
+closeNav.addEventListener("click", () => {
+    nav.classList.remove("nav-modal");
+});
