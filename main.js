990f56229eff9b9576c947753d60b946d97ee8df
Abdurauf1 <aabdurauf004@gmail.com>
2022-12-07 23:02:35 +0500

    fix: recoding responsive navbar

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 "use strict";
 // responsive navbar
 // variables
-const nav = document.querySelector("nav");
-const openNav = document.querySelector(".nav-open-btn");
-const closeNav = document.querySelector(".close-nav-btn");
-openNav.addEventListener("click", openNavFunc);
-function openNavFunc() {
-    nav.classList.add("nav-modal");
-    closeNav.classList.add("display-block");
-}
-closeNav.addEventListener("click", closeNavFunc);
-function closeNavFunc() {
-    nav.classList.remove("nav-modal");
-    closeNav.classList.remove("display-block");
-}
+// const nav = document.querySelector("nav") as HTMLElement;
+// const openNav = document.querySelector(".nav-open-btn") as HTMLButtonElement;
+// const closeNav = document.querySelector(".close-nav-btn") as HTMLButtonElement;
+// openNav.addEventListener("click", openNavFunc);
+// function openNavFunc() {
+//   nav.classList.add("nav-modal");
+// }
+// closeNav.addEventListener("click", closeNavFunc);
+// function closeNavFunc() {
+//   nav.classList.remove("nav-modal");
+// }
--- main.ts
+++ main.ts
@@ -1,19 +1,17 @@
 // responsive navbar
 // variables
-const nav = document.querySelector("nav") as HTMLElement;
-const openNav = document.querySelector(".nav-open-btn") as HTMLButtonElement;
-const closeNav = document.querySelector(".close-nav-btn") as HTMLButtonElement;
+// const nav = document.querySelector("nav") as HTMLElement;
+// const openNav = document.querySelector(".nav-open-btn") as HTMLButtonElement;
+// const closeNav = document.querySelector(".close-nav-btn") as HTMLButtonElement;
 
-openNav.addEventListener("click", openNavFunc);
+// openNav.addEventListener("click", openNavFunc);
 
-function openNavFunc() {
-  nav.classList.add("nav-modal");
-  closeNav.classList.add("display-block");
-}
+// function openNavFunc() {
+//   nav.classList.add("nav-modal");
+// }
 
-closeNav.addEventListener("click", closeNavFunc);
+// closeNav.addEventListener("click", closeNavFunc);
 
-function closeNavFunc() {
-  nav.classList.remove("nav-modal");
-  closeNav.classList.remove("display-block");
-}
+// function closeNavFunc() {
+//   nav.classList.remove("nav-modal");
+// }
