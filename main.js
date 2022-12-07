36a6711c4ef4179c0aef9fbe6281882c0e30adf3
Abdurauf1 <aabdurauf004@gmail.com>
2022-12-08 01:07:07 +0500

    refactor code

--- main.js
+++ main.js
@@ -3,15 +3,13 @@
 const nav = document.querySelector("nav");
 const openNav = document.querySelector(".nav-open-btn");
 const closeNav = document.querySelector(".close-nav-btn");
-openNav.addEventListener("click", openNavFunc);
-function openNavFunc() {
+openNav.addEventListener("click", () => {
     nav.classList.add("nav-modal");
     openNav.classList.add("display-none");
     closeNav.classList.add("display-block");
-}
-closeNav.addEventListener("click", closeNavFunc);
-function closeNavFunc() {
+});
+closeNav.addEventListener("click", () => {
     nav.classList.remove("nav-modal");
     openNav.classList.remove("display-none");
     closeNav.classList.remove("display-block");
-}
+});
--- main.ts
+++ main.ts
@@ -3,18 +3,14 @@ const nav = document.querySelector("nav") as HTMLElement;
 const openNav = document.querySelector(".nav-open-btn") as HTMLButtonElement;
 const closeNav = document.querySelector(".close-nav-btn") as HTMLButtonElement;
 
-openNav.addEventListener("click", openNavFunc);
-
-function openNavFunc() {
+openNav.addEventListener("click", () => {
   nav.classList.add("nav-modal");
   openNav.classList.add("display-none");
   closeNav.classList.add("display-block");
-}
+});
 
-closeNav.addEventListener("click", closeNavFunc);
-
-function closeNavFunc() {
+closeNav.addEventListener("click", () => {
   nav.classList.remove("nav-modal");
   openNav.classList.remove("display-none");
   closeNav.classList.remove("display-block");
-}
+});
