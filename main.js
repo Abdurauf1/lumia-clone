06f92ae6ac18ce5c3c9b70a1e747a565a9d73a43
Abdurauf1 <aabdurauf004@gmail.com>
2022-12-10 20:41:01 +0500

    working on responsie navbar

--- main.js
+++ main.js
@@ -3,6 +3,16 @@
 const nav = document.querySelector("nav");
 const openNav = document.querySelector(".nav-open-btn");
 const closeNav = document.querySelector(".close-nav-btn");
+const dropDown = document.querySelector("#dropDown");
+const deepDropDown = document.querySelector("#deepDropDown");
+const dropDownBtn = document.querySelector("#dropDownBtn");
+const deepDropDownBtn = document.querySelector("#deepDropDownBtn");
+dropDownBtn.addEventListener("click", () => {
+    dropDown.classList.toggle("toggle-drop-down");
+});
+deepDropDownBtn.addEventListener("click", () => {
+    deepDropDown.classList.toggle("toggle-drop-down");
+});
 openNav.addEventListener("click", () => {
     nav.classList.add("nav-modal");
     openNav.classList.add("display-none");
--- main.ts
+++ main.ts
@@ -2,6 +2,18 @@
 const nav = document.querySelector("nav") as HTMLElement;
 const openNav = document.querySelector(".nav-open-btn") as HTMLButtonElement;
 const closeNav = document.querySelector(".close-nav-btn") as HTMLButtonElement;
+const dropDown = document.querySelector("#dropDown") as HTMLUListElement;
+const deepDropDown = document.querySelector("#deepDropDown") as HTMLUListElement;
+const dropDownBtn = document.querySelector("#dropDownBtn") as HTMLAnchorElement;
+const deepDropDownBtn = document.querySelector("#deepDropDownBtn") as HTMLAnchorElement;
+
+dropDownBtn.addEventListener("click", () => {
+  dropDown.classList.toggle("toggle-drop-down");
+});
+
+deepDropDownBtn.addEventListener("click", () => {
+  deepDropDown.classList.toggle("toggle-drop-down");
+});
 
 openNav.addEventListener("click", () => {
   nav.classList.add("nav-modal");
