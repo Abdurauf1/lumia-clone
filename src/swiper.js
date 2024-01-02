3af4957a8f27cf7bb154fea134441d0d2f0573cc
Abdurauf <aabdurauf004@gmail.com>
2024-01-02 17:09:05 +0500

    refactor

--- src/swiper.js
+++ src/swiper.js
@@ -1,19 +1,25 @@
 const swiper = new Swiper('.swiper', {
+    speed: 600,
     loop: true,
     direction: "horizontal",
-    slidesPerView: 1,
-    spaceBetween: 100,
+    slidesPerView: "auto",
     autoplay: {
-        delay: 4000
+        delay: 5000,
+        disableOnInteraction: false
     },
     breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
         1200: {
             slidesPerView: 3,
-            spaceBetween: 0
+            spaceBetween: 20
         }
     },
     pagination: {
         el: '.swiper-pagination',
+        type: "bullets",
         clickable: true,
     },
 }); 
