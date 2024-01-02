99bece95ff4be820101e2ec016fe53f0caf2ffe3
Abdurauf <aabdurauf004@gmail.com>
2024-01-02 17:42:55 +0500

    bug fix

--- dist/main.js
+++ dist/main.js
@@ -155,31 +155,31 @@ cardFilterButtons[0].addEventListener("click", (e) => {
 const testimonialsData = [
     {
         description: "Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus.Accusantium quam, ultricies eget id, aliquam eget nibh et.Maecen aliquam, risus at semper.",
-        img_src: "../assets/img/testimonials/testimonials-1.jpg",
+        img_src: "./assets/img/testimonials/testimonials-1.jpg",
         name: "Saul Goodman",
         job: "Ceo & Founder"
     },
     {
         description: "Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.",
-        img_src: "../assets/img/testimonials/testimonials-2.jpg",
+        img_src: "./assets/img/testimonials/testimonials-2.jpg",
         name: "Sara Wilsson",
         job: "Designer"
     },
     {
         description: "Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.",
-        img_src: "../assets/img/testimonials/testimonials-3.jpg",
+        img_src: "./assets/img/testimonials/testimonials-3.jpg",
         name: "Jena Karlis",
         job: "Store Owner"
     },
     {
         description: "Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.",
-        img_src: "../assets/img/testimonials/testimonials-4.jpg",
+        img_src: "./assets/img/testimonials/testimonials-4.jpg",
         name: "Matt Brandon",
         job: "Freelancer"
     },
     {
         description: "Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.",
-        img_src: "../assets/img/testimonials/testimonials-5.jpg",
+        img_src: "./assets/img/testimonials/testimonials-5.jpg",
         name: "John Larson",
         job: "Entrepreneur"
     },
